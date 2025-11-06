fbfd62f3aba07d7867ad92e6e42bd2aa6e34f9c7
Emilia <01178499@pw.edu.pl>
2025-11-07 00:13:51 +0100

    Add Dockerfile, docker-compose.yaml, add env variables to db.go
    Create Dockerfile and docker-compose.yml and change dsn
    so it uses env variables.
    
    Resolves #4

--- db/db.go
+++ db/db.go
@@ -2,20 +2,30 @@ package db
 
 import (
 	"log"
+	"os"
+	"fmt"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-
-	//"github.com/Emilia-Poleszak/Token_Transfer_API/models"
 )
 
 func ConnectDB() *gorm.DB {
-	// dsn example, modify according to your setup
-	dsn := "host=localhost user=myuser password=mypassword dbname=mydb port=5432 sslmode=disable TimeZone=Europe/Warsaw"
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf(
+    	"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Warsaw",
+    	dbHost, dbUser, dbPass, dbName, dbPort,
+	) 
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	
 	if err != nil {
 		log.Fatalf("Connection failed: %v", err)
 	}
-	//db.AutoMigrate(&models.Wallet{})
+	
 	return db
 }
